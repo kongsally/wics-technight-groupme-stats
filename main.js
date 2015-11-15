99caf43e9bdb07eab3b1d7b3909fc06474cbd60a
Sally Kong <kongsally@users.noreply.github.com>
2015-11-14 22:34:25 -0500

    added jquery and json file

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
-window.onload = initialize;
+message_counts = [];
 
+window.onload = initialize;
 function initialize() {
 	//we will initialize stuff here later
+	$.getJSON("counts.json", function(json) {
+		message_counts = json;
+	});
 }
